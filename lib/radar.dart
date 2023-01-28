d1c1aaa3169addb24261f75a4086540c4ba5ca0b
Jordan <60279731+jordnance@users.noreply.github.com>
2023-01-27 22:13:57 -0800

    Implemented map and radar data overlay

--- lib/radar.dart
+++ lib/radar.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class RadarPage extends StatefulWidget {
   const RadarPage({super.key, required this.title});
   final String title;
-  
+
   @override
   State<RadarPage> createState() => _RadarPageState();
 }
 
 class _RadarPageState extends State<RadarPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: FlutterMap(
+          options: MapOptions(
+            center: LatLng(36.746842, -119.772586),
+            zoom: 5.6,
+          ),
+          nonRotatedChildren: [
+            AttributionWidget.defaultWidget(
+              source: 'OpenStreetMap',
+              onSourceTapped: null,
+            ),
+          ],
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+            ),
+            TileLayer(
+              urlTemplate: 'https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+              backgroundColor: Colors.transparent,
+            )
+          ],
+        ));
   }
 }
