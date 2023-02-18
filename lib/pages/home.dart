5ee36e61a13d6391dedfbb9c43fc20eca52cfc35
Douglas Rank <douglas.rankjr@gmail.com>
2023-02-18 13:13:34 -0800

    mainweather widget moved to external file

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../util/weather_icon_util.dart';
 import '../util/home_utest_util.dart';
+import 'components/mainweather.dart';
 
 ///********************** temporary; generating weather and date values ****************************///
 import 'dart:math';
@@ -111,96 +112,6 @@ class ForecastWeather extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class MainWeather extends StatelessWidget {
-  final int temperature;
-  final String description;
-  final String city;
-  final String date;
-  final int opwKey;
-
-  const MainWeather(
-      this.temperature, this.description, this.city, this.date, this.opwKey,
-      {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    /// ************* temporary data until API calls are brought in ********************** ///
-    ///
-    String units = "°F"; //NOTE: may need to pull from settings
-    String iconKey = HomeTestUtil.utest_opwIconMap[opwKey].toString();
-    String iconName = WeatherIconsUtil.iconMap[iconKey].toString();
-
-    ///
-    /// ***============================================================================*** ///
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        BoxedIcon(WeatherIcons.fromString(iconName),
-            size: 120, color: const Color.fromARGB(255, 87, 87, 87)),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(4.0),
-              child: Text(
-                temperature.toString(),
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                    fontSize: 110,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromARGB(255, 87, 87, 87)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 32),
-              child: Text(
-                units,
-                style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromARGB(255, 87, 87, 87)),
-              ),
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Text(
-            description,
-            style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-                color: Color.fromARGB(255, 87, 87, 87)),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Text(
-            city,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w300,
-                color: Color.fromARGB(255, 87, 87, 87)),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Text(
-            DateFormat('MMM dd').format(DateTime.now()).toString(),
-            style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
-                color: Color.fromARGB(255, 87, 87, 87)),
-          ),
-        ),
-      ],
     );
   }
 }
@@ -446,7 +357,7 @@ class _HomePageState extends State<HomePage> {
               child: PageView(
                 controller: pgController,
                 children: <Widget>[
-                  MainWeather(temp, desc, city, currDay, opwKey),
+                  MainWeather(temp, desc, city, opwKey),
                   MicroWeather(),
                 ],
               ),
