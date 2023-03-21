ce68ec6f706c6a5b95f8cf2732d0328fab8a0ee7
marianal13 <marianalara1320@gmail.com>
2023-03-21 12:46:55 -0700

    Added graphs for pressure and wind

---
+++ lib/pages/components/pressgraph.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class PressGraph extends StatelessWidget {
+  const PressGraph({super.key, required this.todos, required this.duration});
+  final List<FlSpot>? todos;
+  final double? duration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AspectRatio(
+        aspectRatio: 1.4,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            right: 18,
+            left: 12,
+            top: 24,
+            bottom: 12,
+          ),
+          child: LineChart(
+            graphData(duration),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget pressLeftTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+      fontSize: 10,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 28:
+        text = '28';
+        break;
+      case 29:
+        text = '29';
+        break;
+      case 30:
+        text = '30';
+        break;
+      case 31:
+        text = '31';
+        break;
+      case 32:
+        text = '32';
+        break;
+      default:
+        return Container();
+    }
+    return Text(text, style: style, textAlign: TextAlign.center);
+  }
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+      fontSize: 13,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 0:
+        text = const Text('0', style: style);
+        break;
+      case 1:
+        text = const Text('1', style: style);
+        break;
+      case 2:
+        text = const Text('2', style: style);
+        break;
+      case 3:
+        text = const Text('3', style: style);
+        break;
+      case 4:
+        text = const Text('4', style: style);
+        break;
+      case 5:
+        text = const Text('5', style: style);
+        break;
+      case 6:
+        text = const Text('6', style: style);
+        break;
+      case 7:
+        text = const Text('7', style: style);
+        break;
+      case 8:
+        text = const Text('8', style: style);
+        break;
+      case 9:
+        text = const Text('9', style: style);
+        break;
+      case 10:
+        text = const Text('10', style: style);
+        break;
+      case 11:
+        text = const Text('11', style: style);
+        break;
+      case 12:
+        text = const Text('12', style: style);
+        break;
+      case 13:
+        text = const Text('13', style: style);
+        break;
+      case 14:
+        text = const Text('14', style: style);
+        break;
+      default:
+        text = const Text('', style: style);
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: text,
+    );
+  }
+
+  LineChartData graphData(double? maxX) {
+    List<Color> pressColors = [
+      Color.fromARGB(255, 208, 169, 249),
+      Color.fromARGB(255, 115, 1, 143),
+    ];
+
+    return LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: true,
+        horizontalInterval: .5,
+        verticalInterval: 1,
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: Colors.white10,
+            strokeWidth: 1,
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return FlLine(
+            color: Colors.white10,
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        topTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: true, reservedSize: 7),
+          axisNameSize: 20,
+          axisNameWidget: const Text(
+            'Pressure',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w900, fontSize: 18),
+          ),
+        ),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 28,
+            interval: 1,
+            getTitlesWidget: bottomTitleWidgets,
+          ),
+          axisNameWidget: const Text(
+            'Days',
+            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
+          ),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            interval: 1,
+            getTitlesWidget: pressLeftTitleWidgets,
+            reservedSize: 28,
+          ),
+          axisNameWidget: const Text(
+            'Inches of Mercury (inHg)',
+            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
+          ),
+        ),
+      ),
+      borderData: FlBorderData(
+        show: true,
+        border: Border.all(color: const Color(0xff37434d), width: 4),
+      ),
+      minX: 0,
+      maxX: maxX,
+      minY: 27,
+      maxY: 33,
+      lineBarsData: [
+        LineChartBarData(
+          spots: todos,
+          isCurved: false,
+          gradient: LinearGradient(
+            colors: pressColors,
+          ),
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            gradient: LinearGradient(
+              colors:
+                pressColors.map((color) => color.withOpacity(0.3)).toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
