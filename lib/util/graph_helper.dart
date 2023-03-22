03a7f106850ecfe9c1c8f7431ab546b40b19517a
Jordan <60279731+jordnance@users.noreply.github.com>
2023-03-22 14:57:44 -0700

    Attempt at creating averge values

--- lib/util/graph_helper.dart
+++ lib/util/graph_helper.dart
@@ -10,6 +10,7 @@ class GraphHelper {
     var thisTime, difference, parsedLeastCurrent, parsedThisTime;
     double seconds, newX = 0;
     List<double> xCoords = [];
+    bool equalDuration = false;
 
     for (int i = 0; i < test.length; i++) {
       thisTime = test[i]['apiCallDate'];
@@ -18,20 +19,56 @@ class GraphHelper {
       difference = parsedThisTime.difference(parsedLeastCurrent);
       seconds = difference.inSeconds.toDouble();
       newX = seconds / 86400;
-      if (newX <= durationLength!) {
+      if (newX < durationLength!) {
         xCoords.add(newX);
+      } else if (newX == durationLength) {
+        xCoords.add(newX);
+        equalDuration = true;
+      } else if (!equalDuration) {
+        newX = durationLength;
       }
     }
     return xCoords;
   }
 
-  static Future<List<List<double>>> getYCoords() async {
+  static Future<List<List<double>>> getYCoords(double? durationLength) async {
     var test = await SQLHelper.getUserData(SharedPrefUtil.getUserId());
-    double? yRain, yTemp, yHum, ySnow, yWind, yPress;
+    var leastCurrent = test[0]['apiCallDate'];
+
+    var thisTime, difference, parsedLeastCurrent, parsedThisTime;
+    double xValue, seconds, lastX, overX = 0;
+    double? yRain,
+        yTemp,
+        yHum,
+        ySnow,
+        yWind,
+        yPress,
+        lastYRain,
+        lastYTemp,
+        lastYHum,
+        lastYSnow,
+        lastYWind,
+        lastYPress,
+        avgYRain,
+        avgYTemp,
+        avgYHum,
+        avgYSnow,
+        avgYWind,
+        avgYPress;
+
     List<double> values = [];
     List<List<double>> yCoords = [];
+    bool equalDuration = false;
+    bool graphFix = false;
 
     for (int i = 0; i < test.length; i++) {
+      thisTime = test[i]['apiCallDate'];
+      parsedThisTime = DateTime.parse(thisTime);
+      parsedLeastCurrent = DateTime.parse(leastCurrent);
+      difference = parsedThisTime.difference(parsedLeastCurrent);
+      seconds = difference.inSeconds.toDouble();
+      xValue = seconds / 86400;
+
       yRain = test[i]['rain'];
       yRain ??= 0;
       yTemp = test[i]['temp'];
@@ -40,6 +77,34 @@ class GraphHelper {
       ySnow ??= 0;
       yWind = test[i]['windSpeed'];
       yPress = test[i]['pressure'];
+
+      if (xValue == durationLength) {
+        equalDuration = true;
+      }
+
+      if (xValue <= durationLength!) {
+        lastYRain = yRain;
+        lastYTemp = yTemp;
+        lastYHum = yHum;
+        lastYSnow = ySnow;
+        lastYWind = yWind;
+        lastYPress = yPress;
+        values.add(yRain);
+        values.add(yTemp!);
+        values.add(yHum!);
+        values.add(ySnow);
+        values.add(yWind!);
+        values.add(yPress!);
+        lastX = xValue;
+      } else if (!equalDuration && (xValue > durationLength)) {
+        avgYRain = ((yRain + lastYRain!) / 2);
+        avgYTemp = ((yTemp! + lastYTemp!) / 2);
+        avgYHum = ((yHum! + lastYHum!) / 2);
+        avgYSnow = ((ySnow + lastYSnow!) / 2);
+        avgYWind = ((yWind! + lastYWind!) / 2);
+        avgYPress = ((yPress! + lastYPress!) / 2);
+      }
+
       values.add(yRain);
       values.add(yTemp!);
       values.add(yHum!);
@@ -58,7 +123,7 @@ class GraphHelper {
 
       if (test.isNotEmpty) {
         var xCoords = await getXCoords(durationLength);
-        var yCoords = await getYCoords();
+        var yCoords = await getYCoords(durationLength);
         List<List<FlSpot>> newCoords = [];
         List<FlSpot> rainData = [];
         List<FlSpot> tempData = [];
@@ -76,7 +141,15 @@ class GraphHelper {
           pressData.add(FlSpot(xCoords[i], yCoords[i][5]));
         }
 
-        newCoords = [rainData, tempData, humData, snowData, windData, pressData];
+        newCoords = [
+          rainData,
+          tempData,
+          humData,
+          snowData,
+          windData,
+          pressData
+        ];
+
         return newCoords;
       } else {
         return Future.error('No weather data has been stored');
