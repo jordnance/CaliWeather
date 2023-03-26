0e95ac8742339e68931353b804f576fd6b233f1a
Douglas Rank <douglas.rankjr@gmail.com>
2023-03-25 23:45:55 -0700

    added temp lat and long vars for known alerts loc

--- lib/util/globals.dart
+++ lib/util/globals.dart
@@ -2,4 +2,6 @@ library my_prj.globals;
 
 double positionLat = 35.393528;
 double positionLong = -119.043732;
+double alertlat = 34.3917; //TODO: remove after testing
+double alertlong = -118.5426; //TODO: remove after testing
 String apiKey = '0d8187b327e042982d4478dcbf90bae3';
