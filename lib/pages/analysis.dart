ff3142de5101b9335f682fe4770be31f1cf93eb5
Jordan <60279731+jordnance@users.noreply.github.com>
2023-03-28 10:11:56 -0700

    Created dummy data for graphs

--- lib/pages/analysis.dart
+++ lib/pages/analysis.dart
@@ -56,6 +56,153 @@ class _AnalysisPageState extends State<AnalysisPage> {
       wind = data[4];
       press = data[5];
     }
+
+    // Graph dummy data for testing
+    else {
+      List<FlSpot> dummyRain = [
+        const FlSpot(0, 0),
+        const FlSpot(1, 1),
+        const FlSpot(2, 4.5),
+        const FlSpot(3, 3),
+        const FlSpot(4, 4),
+        const FlSpot(5, 2),
+        const FlSpot(6, 2),
+        const FlSpot(7, 1),
+        const FlSpot(8, 2),
+        const FlSpot(9, 1),
+        const FlSpot(10, 2),
+        const FlSpot(11, 4.5),
+        const FlSpot(12, 2),
+        const FlSpot(13, 4),
+        const FlSpot(14, 2),
+      ];
+
+      List<FlSpot> dummyTemp = [
+        const FlSpot(0, 50),
+        const FlSpot(1, 55),
+        const FlSpot(2, 60),
+        const FlSpot(3, 62),
+        const FlSpot(4, 68),
+        const FlSpot(5, 58),
+        const FlSpot(6, 65),
+        const FlSpot(7, 70),
+        const FlSpot(8, 72),
+        const FlSpot(9, 63),
+        const FlSpot(10, 67),
+        const FlSpot(11, 58),
+        const FlSpot(12, 75),
+        const FlSpot(13, 65),
+        const FlSpot(14, 70),
+      ];
+
+      List<FlSpot> dummyHum = [
+        const FlSpot(0, 50),
+        const FlSpot(1, 67),
+        const FlSpot(2, 60),
+        const FlSpot(3, 62),
+        const FlSpot(4, 68),
+        const FlSpot(5, 58),
+        const FlSpot(6, 65),
+        const FlSpot(7, 80),
+        const FlSpot(8, 72),
+        const FlSpot(9, 63),
+        const FlSpot(10, 70),
+        const FlSpot(11, 58),
+        const FlSpot(12, 75),
+        const FlSpot(13, 85),
+        const FlSpot(14, 70),
+      ];
+
+      List<FlSpot> dummySnow = [
+        const FlSpot(0, 0),
+        const FlSpot(1, 1),
+        const FlSpot(2, 3),
+        const FlSpot(3, 3.5),
+        const FlSpot(4, 3),
+        const FlSpot(5, 1),
+        const FlSpot(6, 1),
+        const FlSpot(7, 0.5),
+        const FlSpot(8, 1),
+        const FlSpot(9, 0.5),
+        const FlSpot(10, 1),
+        const FlSpot(11, 2),
+        const FlSpot(12, 3),
+        const FlSpot(13, 2.5),
+        const FlSpot(14, 1),
+      ];
+
+      List<FlSpot> dummyWind = [
+        const FlSpot(0, 0),
+        const FlSpot(1, 1),
+        const FlSpot(2, 4.5),
+        const FlSpot(3, 3),
+        const FlSpot(4, 4),
+        const FlSpot(5, 2),
+        const FlSpot(6, 2),
+        const FlSpot(7, 1),
+        const FlSpot(8, 2),
+        const FlSpot(9, 1),
+        const FlSpot(10, 2),
+        const FlSpot(11, 4.5),
+        const FlSpot(12, 2),
+        const FlSpot(13, 4),
+        const FlSpot(14, 2),
+      ];
+
+      List<FlSpot> dummyPress = [
+        const FlSpot(0, 1000),
+        const FlSpot(1, 1010),
+        const FlSpot(2, 1005),
+        const FlSpot(3, 1020),
+        const FlSpot(4, 1018),
+        const FlSpot(5, 1015),
+        const FlSpot(6, 1022),
+        const FlSpot(7, 1020),
+        const FlSpot(8, 1013),
+        const FlSpot(9, 1010),
+        const FlSpot(10, 1007),
+        const FlSpot(11, 1011),
+        const FlSpot(12, 1009),
+        const FlSpot(13, 1018),
+        const FlSpot(14, 1016),
+      ];
+
+      List<FlSpot> tempData = [];
+      for (int i = 0; i < durationLength! + 1; i++) {
+        tempData.insert(i, dummyRain[i]);
+      }
+      rain = tempData;
+      tempData = [];
+
+      for (int i = 0; i < durationLength + 1; i++) {
+        tempData.insert(i, dummyTemp[i]);
+      }
+      temp = tempData;
+      tempData = [];
+
+      for (int i = 0; i < durationLength + 1; i++) {
+        tempData.insert(i, dummyHum[i]);
+      }
+      hum = tempData;
+      tempData = [];
+
+      for (int i = 0; i < durationLength + 1; i++) {
+        tempData.insert(i, dummySnow[i]);
+      }
+      snow = tempData;
+      tempData = [];
+
+      for (int i = 0; i < durationLength + 1; i++) {
+        tempData.insert(i, dummyWind[i]);
+      }
+      wind = tempData;
+      tempData = [];
+
+      for (int i = 0; i < durationLength + 1; i++) {
+        tempData.insert(i, dummyPress[i]);
+      }
+      press = tempData;
+    }
   }
 
   void changeDuration() {
