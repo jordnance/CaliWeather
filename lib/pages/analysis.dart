aec15953e417e174455b6e446d9c8fcec03737ca
Douglas Rank <douglas.rankjr@gmail.com>
2023-03-27 23:30:25 -0700

    more ui testing; 2 graphs now

--- lib/pages/analysis.dart
+++ lib/pages/analysis.dart
@@ -129,7 +129,8 @@ class _AnalysisPageState extends State<AnalysisPage> {
                         child: PageView(
                           controller: pg2Controller,
                           children: <Widget>[
-                            WindGraph(todos: wind, duration: length),
+                            TempGraph(todos: temp, duration: length),
+                            //WindGraph(todos: wind, duration: length),
                             RainGraph(todos: rain, duration: length),
                             SnowGraph(todos: snow, duration: length),
                           ],
