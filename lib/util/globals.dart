1aee8b20030be9f3d297e5e38b1a3bf2a3401470
Douglas Rank <douglas.rankjr@gmail.com>
2023-04-03 10:35:53 -0700

    created global navbar controller

--- lib/util/globals.dart
+++ lib/util/globals.dart
@@ -1,5 +1,7 @@
 library my_prj.globals;
 
-double alertlat = 34.4480; //TODO: remove after testing
-double alertlong = -119.2429; //TODO: remove after testing
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+
+PersistentTabController navBarController =
+    PersistentTabController(initialIndex: 2);
 String apiKey = '0d8187b327e042982d4478dcbf90bae3';
