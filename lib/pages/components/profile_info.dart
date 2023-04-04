d632faec8bb363db6ba7e75f3f9ef3f1f4b37511
Douglas Rank <douglas.rankjr@gmail.com>
2023-04-04 00:49:40 -0700

    updated border color for modul fields

--- lib/pages/components/profile_info.dart
+++ lib/pages/components/profile_info.dart
@@ -149,7 +149,7 @@ class _ProfileInfoState extends State<ProfileInfo> {
                   color: Colors.white,
                   border: Border.all(
                     width: 1.5,
-                    color: Colors.black,
+                    color: Colors.grey.shade600,
                   ),
                   borderRadius: BorderRadius.circular(10)),
               child: Center(
@@ -199,7 +199,7 @@ class _ProfileInfoState extends State<ProfileInfo> {
                   color: Colors.white,
                   border: Border.all(
                     width: 1.5,
-                    color: Colors.black,
+                    color: Colors.grey.shade600,
                   ),
                   borderRadius: BorderRadius.circular(10)),
               child: Center(
@@ -249,7 +249,7 @@ class _ProfileInfoState extends State<ProfileInfo> {
                   color: Colors.white,
                   border: Border.all(
                     width: 1.5,
-                    color: Colors.black,
+                    color: Colors.grey.shade600,
                   ),
                   borderRadius: BorderRadius.circular(10)),
               child: Center(
