8d5f57dca8d50cc18e97a2a3e6b658d15bea4a34
Douglas Rank <douglas.rankjr@gmail.com>
2023-04-04 14:46:13 -0700

    login moduls updated

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -21,6 +21,7 @@ class _LoginPageState extends State<LoginPage> {
   String? newPasswordValue;
   String? forgotUsernameValue;
   String? forgotPasswordValue;
+  String? confirmedPasswordValue;
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _newFirstNameController = TextEditingController();
@@ -358,56 +359,388 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  // void _forgotPassword() async {
-  //   showModalBottomSheet(
-  //     context: context,
-  //     elevation: 5,
-  //     isScrollControlled: true,
-  //     useRootNavigator: true,
-  //     builder: (_) => Container(
-  //       padding: EdgeInsets.only(
-  //         top: 15,
-  //         left: 15,
-  //         right: 15,
-  //         bottom: MediaQuery.of(context).viewInsets.bottom + 260,
-  //       ),
-  //       child: Column(
-  //         mainAxisSize: MainAxisSize.min,
-  //         crossAxisAlignment: CrossAxisAlignment.end,
-  //         children: [
-  //           TextField(
-  //             controller: _forgotUsernameController,
-  //             decoration: const InputDecoration(hintText: 'Username'),
-  //           ),
-  //           const SizedBox(
-  //             height: 10,
-  //           ),
-  //           TextField(
-  //             controller: _forgotPasswordController,
-  //             decoration: const InputDecoration(hintText: 'Password'),
-  //             obscureText: true,
-  //           ),
-  //           const SizedBox(
-  //             height: 10,
-  //           ),
-  //           ElevatedButton(
-  //             onPressed: () async {
-  //               setState(() {
-  //                 forgotUsernameValue = _forgotUsernameController.text;
-  //                 forgotPasswordValue = _forgotPasswordController.text;
-  //               });
-  //               await _updatePassword();
-  //               if (context.mounted) {
-  //                 Navigator.of(context, rootNavigator: true).pop(context);
-  //               }
-  //             },
-  //             child: const Text('Confirm'),
-  //           )
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
+  void _resigterForm() async {
+    showModalBottomSheet(
+      context: context,
+      elevation: 5,
+      isScrollControlled: true,
+      useRootNavigator: true,
+      backgroundColor: Colors.grey.shade100,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+          top: 15,
+          left: 15,
+          right: 15,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 260,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Center(
+              child: Text(
+                "Register New User",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Divider(
+                thickness: 0.5,
+                color: Colors.grey[600],
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Expanded(
+                  child: SizedBox(
+                    height: 54,
+                    child: TextField(
+                      controller: _newFirstNameController,
+                      obscureText: false,
+                      enabled: true,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(
+                          Icons.person,
+                          size: 16,
+                        ),
+                        floatingLabelAlignment: FloatingLabelAlignment.start,
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                        label: Text.rich(
+                          TextSpan(
+                            children: <InlineSpan>[
+                              WidgetSpan(
+                                child: Text(
+                                  'First Name',
+                                ),
+                              ),
+                              WidgetSpan(
+                                child: Text(
+                                  '*',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 13, 71, 161),
+                            width: 1.5,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 117, 117, 117),
+                              width: 1.5),
+                        ),
+                      ),
+                      textAlign: TextAlign.start,
+                      textDirection: TextDirection.ltr,
+                      onTapOutside: (_) {
+                        FocusScope.of(context).requestFocus(FocusNode());
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Expanded(
+                  child: SizedBox(
+                    height: 54,
+                    child: TextField(
+                      controller: _newLastNameController,
+                      obscureText: false,
+                      enabled: true,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(
+                          Icons.person,
+                          size: 16,
+                        ),
+                        floatingLabelAlignment: FloatingLabelAlignment.start,
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                        label: Text.rich(
+                          TextSpan(
+                            children: <InlineSpan>[
+                              WidgetSpan(
+                                child: Text(
+                                  'Last Name',
+                                ),
+                              ),
+                              WidgetSpan(
+                                child: Text(
+                                  '*',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 13, 71, 161),
+                            width: 1.5,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 117, 117, 117),
+                              width: 1.5),
+                        ),
+                      ),
+                      textAlign: TextAlign.start,
+                      textDirection: TextDirection.ltr,
+                      onTapOutside: (_) {
+                        FocusScope.of(context).requestFocus(FocusNode());
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Expanded(
+                  child: SizedBox(
+                    height: 54,
+                    child: TextField(
+                      controller: _newUsernameController,
+                      obscureText: false,
+                      enabled: true,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(
+                          Icons.abc,
+                          size: 16,
+                        ),
+                        floatingLabelAlignment: FloatingLabelAlignment.start,
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                        label: Text.rich(
+                          TextSpan(
+                            children: <InlineSpan>[
+                              WidgetSpan(
+                                child: Text(
+                                  'Username',
+                                ),
+                              ),
+                              WidgetSpan(
+                                child: Text(
+                                  '*',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 13, 71, 161),
+                            width: 1.5,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 117, 117, 117),
+                              width: 1.5),
+                        ),
+                      ),
+                      textAlign: TextAlign.start,
+                      textDirection: TextDirection.ltr,
+                      onTapOutside: (_) {
+                        FocusScope.of(context).requestFocus(FocusNode());
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Expanded(
+                  child: SizedBox(
+                    height: 54,
+                    child: TextField(
+                      controller: _newPasswordController,
+                      obscureText: true,
+                      enabled: true,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(
+                          Icons.password,
+                          size: 16,
+                        ),
+                        floatingLabelAlignment: FloatingLabelAlignment.start,
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                        label: Text.rich(
+                          TextSpan(
+                            children: <InlineSpan>[
+                              WidgetSpan(
+                                child: Text(
+                                  'Enter Password',
+                                ),
+                              ),
+                              WidgetSpan(
+                                child: Text(
+                                  '*',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 13, 71, 161),
+                            width: 1.5,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 117, 117, 117),
+                              width: 1.5),
+                        ),
+                      ),
+                      textAlign: TextAlign.start,
+                      textDirection: TextDirection.ltr,
+                      onTapOutside: (_) {
+                        FocusScope.of(context).requestFocus(FocusNode());
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Expanded(
+                  child: SizedBox(
+                    height: 54,
+                    child: TextField(
+                      controller: _confirmPasswordController,
+                      obscureText: true,
+                      enabled: true,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(
+                          Icons.password,
+                          size: 16,
+                        ),
+                        floatingLabelAlignment: FloatingLabelAlignment.start,
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                        label: Text.rich(
+                          TextSpan(
+                            children: <InlineSpan>[
+                              WidgetSpan(
+                                child: Text(
+                                  'Confirm Password',
+                                ),
+                              ),
+                              WidgetSpan(
+                                child: Text(
+                                  '*',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 13, 71, 161),
+                            width: 1.5,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 117, 117, 117),
+                              width: 1.5),
+                        ),
+                      ),
+                      textAlign: TextAlign.start,
+                      textDirection: TextDirection.ltr,
+                      onTapOutside: (_) {
+                        FocusScope.of(context).requestFocus(FocusNode());
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 96, 96, 96),
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _clearTextControllers();
+                    });
+                    Navigator.of(context, rootNavigator: true).pop(context);
+                  },
+                  child: const Text('Cancel'),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 0, 83, 129),
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onPressed: () async {
+                    setState(() {
+                      newFirstNameValue = _newFirstNameController.text;
+                      newLastNameValue = _newLastNameController.text;
+                      newUsernameValue = _newUsernameController.text;
+                      newPasswordValue = _newPasswordController.text;
+                      confirmedPasswordValue = _newPasswordController.text;
+                    });
+                    await _addUser();
+                    if (context.mounted) {
+                      Navigator.of(context, rootNavigator: true).pop(context);
+                    }
+                    showMessage("Welcome to the team!");
+                  },
+                  child: const Text('Sign Up'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   Future<void> _updatePassword() async {
     if (_forgotPasswordController.text == '' ||
@@ -438,7 +771,7 @@ class _LoginPageState extends State<LoginPage> {
     setState(() {});
   }
 
-  void _resigterForm() async {
+  void _resigterForm1() async {
     showModalBottomSheet(
         context: context,
         elevation: 5,
@@ -496,7 +829,6 @@ class _LoginPageState extends State<LoginPage> {
                       if (context.mounted) {
                         Navigator.of(context, rootNavigator: true).pop(context);
                       }
-                      showMessage("You've successfully signed up!");
                     },
                     child: const Text('Sign Up'),
                   )
@@ -506,12 +838,34 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Future<void> _addUser() async {
+    if (_newPasswordController.text == '' ||
+        _confirmPasswordController.text == '' ||
+        _newUsernameController.text == '' ||
+        _newFirstNameController.text == '' ||
+        _newLastNameController.text == '') {
+      _clearTextControllers();
+      showMessage('Fields cannot be empty');
+      return;
+    }
+
+    if (_newPasswordController.text != _confirmPasswordController.text) {
+      _clearTextControllers();
+      showMessage('Passwords do not match');
+      return;
+    }
+
+    var user = await SQLHelper.getUserByUsername(_newUsernameController.text);
+    if (user.isNotEmpty) {
+      _clearTextControllers();
+      showMessage("Username already exists");
+      return;
+    }
+
     SQLHelper.createUser(newFirstNameValue, newLastNameValue, newUsernameValue,
         newPasswordValue);
-    _newFirstNameController.text = '';
-    _newLastNameController.text = '';
-    _newUsernameController.text = '';
-    _newPasswordController.text = '';
+    _clearTextControllers();
+    showMessage("You've successfully signed up!");
+    setState(() {});
   }
 
   @override
